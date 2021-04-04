d1a85ab2132ebc8a3a5e3f3135d293ef7cc46a63
= <alexoo@inbox.ru>
2021-04-04 22:43:32 +0300

    Interface, empty interface

---
+++ Beginner/theory/14.Interfaces, Empty interfaces/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+type Sequence []int
+
+// Methods required by sort.Interface.
+func (s Sequence) Len() int {
+	return len(s)
+}
+func (s Sequence) Less(i, j int) bool {
+	return s[i] < s[j]
+}
+func (s Sequence) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+
+// Copy returns a copy of the Sequence.
+func (s Sequence) Copy() Sequence {
+	copy := make(Sequence, 0, len(s))
+	return append(copy, s...)
+}
+
+// Method for printing - sorts the elements before printing.
+func (s Sequence) String() string {
+	s = s.Copy() // Make a copy; don't overwrite argument.
+	sort.Sort(s)
+	str := "["
+	for i, elem := range s { // Loop is O(N²); will fix that in next example.
+		if i > 0 {
+			str += " "
+		}
+		str += fmt.Sprint(elem)
+	}
+	return str + "]"
+}
+
+func main() {
+
+}
